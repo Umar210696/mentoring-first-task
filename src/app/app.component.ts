39b5c76c126969c1c77744a97f9def7bdf1c26ae
УмарАР96 <umarmansurovich@gmail.com>
2024-12-06 17:54:57 +0200

    prototip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+
+  readonly headeritem1 = 'Каталог';
+
+  readonly headeritem2 = 'О компании';
+
+  readonly headeritem3 = 'Каталог';
+
+
+  readonly header2item1 = 'Каталог'
+
+  readonly header2item2 = 'Стройматериалы'
+
+  readonly header2item3 = 'Инструменты'
+
+  readonly header2item4 = 'Электрика'
+
+  readonly header2item5 = 'Интерьер и одежда'
+
 }
